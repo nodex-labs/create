118d22591a6cec7040acf7a132ee6811529f5355
Albert Arakelyan <albertarakelyan1998@gmail.com>
2025-03-30 12:27:59 +0400

    feat(3): created basic app

---
+++ constants/frameworks.js
@@ -0,0 +1,12 @@
+const NODE_FRAMWORKS = [
+  {
+    name: 'Express.js',
+    value: 'express',
+    description: 'Node.js web minimalistic framework',
+    color: 'green',
+  },
+];
+
+module.exports = {
+  NODE_FRAMWORKS,
+};
